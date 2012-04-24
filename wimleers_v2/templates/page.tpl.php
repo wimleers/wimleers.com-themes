fd2e204d3357cdbe232955b8489eaf8702f6b125
Xavier Bertels <github@xavierbertels.com>
2012-04-25 00:48:39 +0200

    add extra div for tabsContent

--- wimleers_v2/templates/page.tpl.php
+++ wimleers_v2/templates/page.tpl.php
@@ -126,7 +126,9 @@
       <?php if ($tabs): ?><div class="tabs"><?php print render($tabs); ?></div><?php endif; ?>
       <?php print render($page['help']); ?>
       <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
+	  <?php if($tabs): ?><div class="tabsContent"><?php endif; ?>
       <?php print render($page['content']); ?>
+	  <?php if($tabs): ?></div><?php endif; ?>
     </article>
     </div>
   </section>
