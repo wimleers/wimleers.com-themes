62877ef03d94a8890b8a9683e4f0496eacea4e78
Wim Leers <work@wimleers.com>
2012-07-01 19:51:16 +0200

    Remove tab cruft introduced in dac7f41, fd2e204, 6d94f98.

--- wimleers_v2/templates/page.tpl.php
+++ wimleers_v2/templates/page.tpl.php
@@ -126,9 +126,7 @@
       <?php if ($tabs): ?><div class="tabs"><?php print render($tabs); ?></div><?php endif; ?>
       <?php print render($page['help']); ?>
       <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
-	  <?php if($tabs): ?><div class="tabsContent body"><?php endif; ?>
       <?php print render($page['content']); ?>
-	  <?php if($tabs): ?></div><?php endif; ?>
     </article>
     </div>
   </section>
@@ -158,7 +156,7 @@
     </aside>
 
     <?php if ($node): ?>
-      <?php if ($tabs): ?><div class="tabs"><?php print render($tabs); ?></div><?php endif; ?>
+      <?php if ($tabs): ?><div class="tabs g9"><?php print render($tabs); ?></div><?php endif; ?>
       <?php print render($page['help']); ?>
       <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
       <?php print render($page['content']); ?>
@@ -169,7 +167,7 @@
           <?php if ($title): ?><h1><?php print $title; ?></h1><?php endif; ?>
           <?php print render($title_suffix); ?>
         </header>
-        <?php if ($tabs): ?><div class="tabs"><?php print render($tabs); ?></div><?php endif; ?>
+        <?php if ($tabs): ?><div class="tabs g9"><?php print render($tabs); ?></div><?php endif; ?>
         <?php print render($page['help']); ?>
         <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
         <?php print render($page['content']); ?>
