40f19c338f2aca07d9ec7a23952680384857f137
Wim Leers <work@wimleers.com>
2017-04-05 13:38:46 +0200

    PHP 7 compatibility. This was causing an Error to be thrown, with the message "Cannot pass parameter 1 by reference".

--- wimleers_v2/templates/page.tpl.php
+++ wimleers_v2/templates/page.tpl.php
@@ -167,7 +167,7 @@
           <?php if (isset($title)): ?><h1><?php print $title; ?></h1><?php endif; ?>
           <?php print render($title_suffix); ?>
         </header>
-        <?php if ($rendered_tabs = render(isset($tabs) ? $tabs : array())): ?><div class="tabs g9"><?php print $rendered_tabs; ?></div><?php endif; ?>
+        <?php if (isset($tabs)): ?><div class="tabs g9"><?php print render($tabs); ?></div><?php endif; ?>
         <?php print render($page['help']); ?>
         <?php if (isset($action_links)): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
         <?php print render($page['content']); ?>
